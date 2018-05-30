806d906fcbffb24314460798436471a28d2bf0aa
Rodrigo Vazquez <rodrigovazquez420@gmail.com>
2018-05-30 19:25:04 -0300

    intento de solucionar el tablero con una matriz

--- tp5/ej7/TableroMagico.java
+++ tp5/ej7/TableroMagico.java
@@ -2,69 +2,64 @@ package ej7;
 
 import java.util.ArrayList;
 
-import nReinas.NReinas.Position;
-
 public class TableroMagico {
 
-	class Position {
-		int row, col, val;
-		Position(int row, int col, int val) {
-			this.row = row;
-			this.col = col;
-			this.val = val;
-		}
-	}
-
-	public Position[] resolverTablero(int n, int k, int s) {	//n = tablero de n*n - k>n*n
+	public int[][] resolverTablero(int n, int k, int s) {	//n = tablero de n*n - k>n*n
 		if (k>n*n) {
-			Position[] vectorResultados = new Position[n*n];
+			int[][] matrizSolucion = new int[n][n];
 			int[] sumaFila = new int[n];
 			int[] sumaCol = new int[n];
-			
+
 			for (int i = 0; i < n; i++) {
 				sumaFila[i] = 0;
 				sumaCol[i] = 0;
 			}
-			
+
 			ArrayList<Integer> opc = new ArrayList<Integer>();
 			for (int i = 1; i <= k; i++) {
 				opc.add(i);
 			}
-			boolean hasSolution = resolverTableroBack(n, k, s, vectorResultados, opc, 0, sumaFila, sumaCol);
+			boolean hasSolution = resolverTableroBack(n, k, s, matrizSolucion, opc, 0, 0, sumaFila, sumaCol);
 			if (hasSolution) {
-				return vectorResultados;
+				return matrizSolucion;
 			} 
 		}
-		return new Position[0];
+		return new int[0][0];
 	}
-	
-	public boolean resolverTableroBack(int n, int k, int s, Position[] e, ArrayList<Integer> opc, int row, int[] sumaFila, int[] sumaCol  ) {
-		if (n == row) {
-            return true;
-        }
-		
-		int col;
-        for (col = 0; col < n; col++) {
-        	boolean foundSafe = true;
-        	int valor = opc.get(col);
-        	if(sumaFila[row]+valor <= s && sumaCol[col]+valor <= s) {
-        		e[(n*row)+col] = new Position(row, col, opc.remove(col));
-        		sumaFila[row] +=  valor;
-        		sumaCol[col] +=  valor;
-        	} else {
-        		foundSafe = false;
-        	}
-        	if (foundSafe) {
-        		
-                if (resolverTableroBack(n, k, s, e, opc, row+1, sumaFila, sumaCol)) {
-                    return true;
-                }
-            }
-        	
-        }
+
+	public boolean resolverTableroBack(int n, int k, int s, int[][] matriz, ArrayList<Integer> opc, int row, int col, int[] sumaFila, int[] sumaCol  ) {
+		if (solucion(sumaFila, sumaCol, s)) {
+			return true;
+		}else{
+			for (int i = row; i < n; i++) {
+				for (int j = col; j < n; j++) {
+					if (matriz[i][j] == 0) {
+						matriz[i][j] = opc.remove(0);
+						sumaFila[col] += matriz[i][j];
+						sumaFila[row] += matriz[i][j];
+						
+						if (resolverTableroBack(n, k, s, matriz, opc, row+1, col+1, sumaFila, sumaCol)) {
+							
+						}
+					}
+				}
+
+			}
+		}
+
 		return false;
 	}
-	
+
+	public boolean solucion(int[] sumaFila, int[] sumaCol, int s) {
+		boolean esSol=true;
+		for (int i = 0; i < sumaCol.length; i++) {
+			if(sumaCol[i] != s && sumaFila[i] != s) {
+				return false;
+			}
+		}
+		return esSol;
+	}
+
 	public static void main(String args[]) {
 
 	}
